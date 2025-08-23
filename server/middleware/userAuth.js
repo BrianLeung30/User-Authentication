43c41b4a2640db7475bb14dc0287e691b5e06ef0
BrianLeung123 <bleung0112@gmail.com>
2025-08-23 15:06:10 -0700

    Set up userAuth API

---
+++ server/middleware/userAuth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+const userAuth = async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return res.json({ success: false, message: "Unauthorized" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (decoded.id) {
+      if (!req.body) {
+        req.body = {};
+      }
+      req.body.userId = decoded.id;
+    } else {
+      return res.json({ success: false, message: "Unauthorized" });
+    }
+
+    next();
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+export default userAuth;
