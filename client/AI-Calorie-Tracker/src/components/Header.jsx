a769d2b0345d30443e1b261aaa152ac0125dd613
BrianLeung123 <bleung0112@gmail.com>
2025-08-23 19:14:30 -0700

    Created sign up and login form

---
+++ client/AI-Calorie-Tracker/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex flex-col items-center mt-20 px-4 text-center text-gray-800">
+      <h1 className="flex items-center gap-2 text-xl sm:text-3xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, aut.
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
